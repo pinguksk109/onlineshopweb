d89a8bd0fda505c74e0020e5f5a8d42a15adab92
Keisuke Ota <41576749+pinguksk109@users.noreply.github.com>
2020-02-22 07:18:13 +0900

    Add files via upload

---
+++ web/kakunin.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <script src="main.js"></script>
+    <title>SHOP</title>
+</head>
+
+<header>
+	<!--logo画像-->
+	<a href="index.html"><img src="shop.png"></a>
+</header>
+
+<doby>
+
+<br><br><br>
+
+<a href="tops.html" class="btn-border-bottom">トップス</a>&nbsp;&nbsp;&nbsp;
+<a href="jacket.html" class="btn-border-bottom">ジャケット</a>&nbsp;&nbsp;&nbsp;
+<a href="pants.html" class="btn-border-bottom">パンツ</a>&nbsp;&nbsp;&nbsp;
+<a href="shoes.html" class="btn-border-bottom">シューズ</a>
+
+<br><br>
+
+<a>以下の通りで受けたまりました。</a>
+
+<?php
+
+	//綺麗にしたい
+	print $_POST['mono'];
+	print $_POST['name_text'];
+	print $_POST['address_text'];
+	print $_POST['pay'];
+
+	$mono = $_POST['mono'];
+	$name_text = $_POST['name_text'];
+	$address_text = $_POST['address_text'];
+	$pay = $_POST['pay']
+	
+?>
+
+<h3>ご利用ありがとうございました。</h3>
+
+<a href="index.html" class="btn-gradient-radius">トップに戻る</a>
+
+</doby>
+</html>
---
+++ web/tyumon.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <script src="main.js"></script>
+    <title>SHOP</title>
+</head>
+
+<header>
+	<!--logo画像-->
+	<a href="index.html"><img src="shop.png"></a>
+</header>
+
+<doby>
+
+<br><br><br>
+
+
+
+<a href="tops.html" class="btn-border-bottom">トップス</a>&nbsp;&nbsp;&nbsp;
+<a href="jacket.html" class="btn-border-bottom">ジャケット</a>&nbsp;&nbsp;&nbsp;
+<a href="pants.html" class="btn-border-bottom">パンツ</a>&nbsp;&nbsp;&nbsp;
+<a href="shoes.html" class="btn-border-bottom">シューズ</a>
+
+<br><br>
+
+<form name="shop_form" method = "POST" action="kakunin.php">
+
+<h3>選んだ服を選択してください</h3>
+<input type="radio" name="mono" value="1">トップス1
+<input type="radio" name="mono" value="2">トップス2
+<br>
+<input type="radio" name="mono" value="3">ジャケット1
+<input type="radio" name="mono" value="4">ジャケット2
+<br>
+<input type="radio" name="mono" value="5">パンツ1
+<input type="radio" name="mono" value="6">パンツ2
+<br>
+<input type="radio" name="mono" value="7">シューズ1
+<input type="radio" name="mono" value="8">シューズ2
+<br>
+
+
+<h3>名前を入力してください</h3>
+お名前：<input type="text" name="name_text">
+
+<h3>住所を入力してください</h3>
+住所：<input type="text" name="address_text">
+
+<h3>支払い方法を選択してください</h3>
+<input type="radio" name="pay" value="1">コンビニ支払い
+<input type="radio" name="pay" value="2">着払い
+<p>コンビニ支払いは、ローソンとファミリーマートで支払いが可能です。<br>着払いは、着払手数料として300円かかります。</p>
+
+<input type="submit" name="add" value="買う" class="btn-gradient-radius">
+
+<br><br>
+
+<a href="index.html" class="btn-gradient-radius">トップに戻る</a>
+
+</doby>
+</html>
